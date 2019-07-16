829aa8f49d49cb1b8f61a1999542e3abc5d336d5
Stefanos Chrs <root@stefanoschrs.com>
2019-07-16 12:28:25 +0200

    :cactus:

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('Hello World')
+const { parse } = require('url')
+
+module.exports = async function (req, res) {
+  try {
+    const { pathname = '/' } = parse(req.url)
+
+    res.setHeader('Content-Type', 'application/json')
+    res.status(200).send({
+      pathname
+    })
+  } catch (e) {
+    console.error(e.message)
+
+    res.setHeader('Content-Type', 'text/html')
+    res.status(500).send('<h1>Unexpected Error</h1><p>Sorry, there was a problem</p>')
+  }
+}
